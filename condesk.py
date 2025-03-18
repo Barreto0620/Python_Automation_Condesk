18703a5aa6d6cc5a1a3d45c09f5afb3a803291e9
Gabriel Barreto <gabrielprozds@gmail.com>
2025-03-18 10:04:10 -0300

    Projeto Finalizado !

--- condesk.py
+++ condesk.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 # Pause Global
 
-pyautogui.PAUSE = 1.3
+pyautogui.PAUSE = 1.3   
 
 # Passo 1: Entrar no Condesk
 pyautogui.press("win")
@@ -18,17 +18,17 @@ pyautogui.write("https://hemc.consulters.com.br/")
 pyautogui.press("enter")
 
 # Passo 2: Fazer login
-pyautogui.write("sigiloso")
-pyautogui.press("tab")
-pyautogui.write(str("sigiloso"))
+#pyautogui.write("sigiloso")
+#pyautogui.press("tab")
+#pyautogui.write(str("sigiloso"))
 pyautogui.press("enter")
 time.sleep(2)
  
 # Passo 3: Acessar o Painel de Criação do Usuário
-pyautogui.click(x=82, y=245)
-pyautogui.click(x=61, y=287)
+pyautogui.click(x=96, y=274)
+pyautogui.click(x=52, y=312)
 time.sleep(1.5)
-pyautogui.click(x=311, y=237)
+pyautogui.click(x=301, y=264)
 time.sleep(1.5)
 
 # Passo 3: Importando a base de dados para cadastro
@@ -51,45 +51,50 @@ for linha in tabela.index:
     pyautogui.write(tabela.loc[linha, "email"])
     pyautogui.press("tab", presses=2)
     pyautogui.write(tabela.loc[linha, "area"])
-    time.sleep(0.10)
+    pyautogui.hotkey("ctrl","a")
+    pyautogui.hotkey("ctrl","c")
+    time.sleep(0.20)
     pyautogui.press("down")
     pyautogui.press("tab")
     pyautogui.write("HEMC") 
-    time.sleep(0.10)
+    time.sleep(0.20)
     pyautogui.press("tab")
-    pyautogui.write("000000 - Genérico")
-    time.sleep(0.10)
-    pyautogui.press("tab")
-    pyautogui.write("Usuario")
-    time.sleep(0.10)
+    pyautogui.hotkey("ctrl","v")
+    time.sleep(0.20)
     pyautogui.press("tab")
     pyautogui.write("Usuario")
-    time.sleep(0.10)
+    time.sleep(0.20)
+    pyautogui.press("tab")
+    pyautogui.write("Usuario")
+    time.sleep(0.20)
     pyautogui.press("tab", presses=3)
-    pyautogui.click(x=553, y=573)
+    pyautogui.click(x=554, y=604)
     pyautogui.press("tab", presses=3)
     pyautogui.write(str(tabela.loc[linha, "senha"]))
     pyautogui.press("tab", presses=2)
     pyautogui.press("enter")
     time.sleep(1.5)
-    pyautogui.click(x=1131, y=598)
-    time.sleep(2)
-    pyautogui.click(x=327, y=234)
+    pyautogui.click(x=1092, y=612)
     time.sleep(2)
     #Usuário Criado
 
-pyautogui.alert('Cadastro Finalizado !!') 
+    # Passo 5: Fechar o Navegador
+    pyautogui.alert('Cadastro Finalizado !!')
+    time.sleep(2.5)
+    pyautogui.press("enter")
+    pyautogui.hotkey('alt', 'f4') 
 
 #Abrindo planilha
 pyautogui.press("win")
-pyautogui.write("Documentos: condesk_register.xlsx")
+pyautogui.write(r"C:\Users\gabriel.oliveira\Documents\condesk_register.xlsx")
 time.sleep(1)
 pyautogui.press("enter")
 time.sleep(4)
+pyautogui.hotkey('win', 'up')
 
 #Atualizando informações
 pyautogui.hotkey('ctrl', 'pageup')
-pyautogui.click(x=17, y=256)
+pyautogui.click(x=16, y=268)
 pyautogui.dragTo(x=16, y=598, duration=1)
 pyautogui.hotkey("ctrl","x")
 pyautogui.hotkey('ctrl', 'pagedown')
